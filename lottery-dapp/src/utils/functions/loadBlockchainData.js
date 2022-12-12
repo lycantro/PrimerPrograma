3d37b693fca9fad01ba5db0247d62128a5d61807
juanor940423 <consultoriappmovil@gmail.com>
2022-12-12 17:50:14 -0500

    Files order to frontend

---
+++ lottery-dapp/src/utils/functions/loadBlockchainData.js
@@ -0,0 +1,40 @@
+import contrato_loteria from "../../../build/contracts/Prediction.json";
+
+const loadBlockchainData = async () => {
+  const networkGame = 97;
+  const web3 = window.web3;
+  //carga de la cuenta
+  let contract;
+  let contract_BUSD;
+  const accounts = await web3.eth.getAccounts();
+  console.log("Account: ", accounts[0]);
+  const networkId = "97"; //---------------------------------------aca se cambia segun la red donde se despliegue ganache: 5777 BscTestnet: 97
+  console.log("NetworkId: ", networkId);
+  const networkData = contrato_loteria.networks[networkId];
+  console.log("NetworkData: ", networkData);
+  const network = await web3.eth.getChainId();
+
+  if (network != networkGame) {
+    await window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: "0x61" }], // chainId must be in hexadecimal numbers
+    });
+  }
+
+  if (networkData) {
+    const abi = contrato_loteria.abi;
+    console.log("Abi: ", abi);
+    const address = networkData.address;
+    console.log("Address SC: ", address);
+    contract = new web3.eth.Contract(abi, address); //asi se instancia el contrato
+  } else {
+    window.alert("El smart contrat no se ha desplegado en la red");
+  }
+
+  return {
+    account: accounts[0],
+    contract: contract ?? "Contract error",
+  };
+};
+
+export default loadBlockchainData;
