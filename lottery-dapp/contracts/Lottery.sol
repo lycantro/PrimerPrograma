cf03388c7d2b9341eccaeddca522e8133d7866cf
Juan Manuel <consultoriappmovil@gmail.com>
2022-12-12 12:30:58 -0500

    Updates Web3

--- lottery-dapp/contracts/Lottery.sol
+++ lottery-dapp/contracts/Lottery.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
