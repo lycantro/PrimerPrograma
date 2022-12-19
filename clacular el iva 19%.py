6b1a521ba09b0140b6d5d71a39674c9ef6ccac47
leonardo rincon <leonardorincon0092@gmail.com>
2022-12-18 21:42:20 -0500

    resolvi el problema que tenia con los numeros individuales de el premio en una linea de numeros falta agragar la resta del random

---
+++ clacular el iva 19%.py
@@ -0,0 +1,10 @@
+def calcular_iva(n1):
+    iva= (n1*0.19)
+    return iva
+
+
+ingrresa=float(input("ingresa la cantidad a calcular "))
+
+calcular_iva(ingrresa)
+
+print(" el iva de su producto es",calcular_iva(ingrresa))
